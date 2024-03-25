dd64d1b333bfbd91a384a24df42115581793da88
Pedro Uchoa <joaopedrouchoacampos@gmail.com>
2024-03-24 22:34:39 -0300

    Add: footer

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { CardsContainerComponent } from '../../components/cards-container/cards-container.component';
 import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeaderComponent,CardsContainerComponent],
+  imports: [HeaderComponent,CardsContainerComponent,FooterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
