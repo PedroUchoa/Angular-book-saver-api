3418a5c7bd0032fccb57d511ef8d40982900e870
PedroUchoa <joaopedrouchoacampos@gmail.com>
2024-04-11 17:54:39 -0300

    Create: Property Biding and Interfaces

--- src/app/components/cards-container/cards-container.component.ts
+++ src/app/components/cards-container/cards-container.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardsComponent } from '../cards/cards.component';
+import { ICards } from '../../interface/ICards.interface';
 
 @Component({
   selector: 'app-cards-container',
   standalone: true,
   imports: [CardsComponent],
   templateUrl: './cards-container.component.html',
-  styleUrl: './cards-container.component.css'
+  styleUrl: './cards-container.component.css',
 })
 export class CardsContainerComponent {
-
+  public arrayCards = signal<ICards[]>([
+    {
+      id: 'dae558f6-10c1-462d-b451-e68bf761decd',
+      name: 'Duna: livro 1',
+      author: 'Frank Herbert',
+      description:
+        'Uma estonteante mistura de aventura e misticismo, ecologia e política, este romance ganhador dos prêmios Hugo e Nebula deu início a uma das mais épicas histórias de toda a ficção científica. Duna é um triunfo da imaginação, que influenciará a literatura para sempre.Esta edição inédita, com introdução de Neil Gaiman, apresenta ao leitor o universo fantástico criado por Herbert e que será adaptado ao cinema por Denis Villeneuve, diretor de A chegada e de Blade Runner 2049.',
+      image: 'https://m.media-amazon.com/images/I/81zN7udGRUL._SY466_.jpg',
+      categories: ['SCI_FI', 'FANTASIA'],
+    },
+  ]);
 }
---
+++ src/app/interface/ICards.interface.ts
@@ -0,0 +1,8 @@
+export interface ICards{
+  id:string,
+  name:string,
+  author:string,
+  description:string,
+  image:string,
+  categories:string[],
+}
